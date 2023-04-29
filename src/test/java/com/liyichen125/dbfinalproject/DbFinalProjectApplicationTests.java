6068598381165230d872211f8439af6b5879eadb
liyichen125 <friday50523@gmail.com>
2023-04-29 16:37:09 +0800

    initial

---
+++ src/test/java/com/liyichen125/dbfinalproject/DbFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liyichen125.dbfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
