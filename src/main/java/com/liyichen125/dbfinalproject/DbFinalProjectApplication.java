02383b5ea4c22c8263fb508eb6150b4b925afeeb
liyichen125 <friday50523@gmail.com>
2023-04-29 16:37:09 +0800

    initial

---
+++ src/main/java/com/liyichen125/dbfinalproject/DbFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.liyichen125.dbfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbFinalProjectApplication.class, args);
+	}
+
+}
