cc5fee1a3d5225180c631c75f9a2818cbfc4c463
liyichen125 <friday50523@gmail.com>
2023-05-26 21:20:34 +0800

    register (modified)

--- src/main/java/com/liyichen125/dbfinalproject/dto/UserRegisterRequest.java
+++ src/main/java/com/liyichen125/dbfinalproject/dto/UserRegisterRequest.java
@@ -9,7 +9,54 @@ public class UserRegisterRequest {
     @NotNull
     private Integer user_id;
     @NotNull
+    private String name;
+    private Integer role;
+    private String phoneNum;
+    private String roomNum;
+    private String department_grade;
+    @NotNull
     private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getRole() {
+        return role;
+    }
+
+    public void setRole(Integer role) {
+        this.role = role;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getRoomNum() {
+        return roomNum;
+    }
+
+    public void setRoomNum(String roomNum) {
+        this.roomNum = roomNum;
+    }
+
+    public String getDepartment_grade() {
+        return department_grade;
+    }
+
+    public void setDepartment_grade(String department_grade) {
+        this.department_grade = department_grade;
+    }
+
 
     public Integer getUser_id() {
         return user_id;
--- src/main/java/com/liyichen125/dbfinalproject/service/impl/UserServiceImpl.java
+++ src/main/java/com/liyichen125/dbfinalproject/service/impl/UserServiceImpl.java
@@ -4,11 +4,16 @@ import com.liyichen125.dbfinalproject.dao.UserDao;
 import com.liyichen125.dbfinalproject.dto.UserRegisterRequest;
 import com.liyichen125.dbfinalproject.model.User;
 import com.liyichen125.dbfinalproject.service.UserService;
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
     @Autowired//使用InterFace 發揮spring Ioc特性
     private UserDao userDao;
 
@@ -19,6 +24,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        // 避免重複註冊
+        User user = userDao.getUserById(userRegisterRequest.getUser_id());
+        if(user != null){
+            log.warn("該id已經被使用");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        // 成功創建帳號
         return userDao.createUser(userRegisterRequest);
     }
 }
