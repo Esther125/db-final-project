b0e626565460da452831d4f83044552ac5f56a58
liyichen125 <friday50523@gmail.com>
2023-05-28 23:03:06 +0800

    login with welcome page

--- src/main/java/com/liyichen125/dbfinalproject/DbFinalProjectApplication.java
+++ src/main/java/com/liyichen125/dbfinalproject/DbFinalProjectApplication.java
@@ -2,8 +2,10 @@ package com.liyichen125.dbfinalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.liyichen125.dbfinalproject")
 public class DbFinalProjectApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,5 @@ public class DbFinalProjectApplication {
 	}
 
 }
+
+
